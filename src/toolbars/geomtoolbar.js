e541378fddb931f29164f71a5f2b81ee703544c2
Benjamin Nortier <bjnortier@gmail.com>
2013-09-04 08:52:04 +0200

    Issue #4 - Prohibit selection when editing

--- src/toolbars/geomtoolbar.js
+++ src/toolbars/geomtoolbar.js
@@ -1,109 +1,109 @@
 define([
-    'toolbars/toolbar', 
-    'commandstack', 
-    'interactioncoordinator',
-    'selection',
-    'modelviews/vertexMV',
-    'modelviews/modelgraph',
-    ], 
-    function(
-        toolbar, 
-        commandStack, 
-        coordinator, 
-        selection,
-        VertexMV,
-        modelGraph) {
+  'toolbars/toolbar', 
+  'commandstack', 
+  'interactioncoordinator',
+  'selection',
+  'modelviews/vertexMV',
+  'modelviews/modelgraph',
+  ], 
+  function(
+    toolbar, 
+    commandStack, 
+    coordinator, 
+    selection,
+    VertexMV,
+    modelGraph) {
 
-    var SelectItemModel = toolbar.ItemModel.extend({
-        
-        name: 'select',
+  var SelectItemModel = toolbar.ItemModel.extend({
+    
+    name: 'select',
 
-        activate: function() {
-            toolbar.ItemModel.prototype.activate.call(this);
-            selection.canSelect = true;
-        },
+    activate: function() {
+      toolbar.ItemModel.prototype.activate.call(this);
+      selection.canSelect = true;
+    },
 
-        deactivate: function() {
-            toolbar.ItemModel.prototype.deactivate.call(this);
-            selection.canSelect = false;
-            selection.deselectAll();
-        },
+    deactivate: function() {
+      toolbar.ItemModel.prototype.deactivate.call(this);
+      selection.canSelect = false;
+      selection.deselectAll();
+    },
 
-    });
+  });
 
-    var selectItemModel   = new SelectItemModel();
+  var selectItemModel   = new SelectItemModel();
 
-    var GeomToolbarModel = toolbar.Model.extend({
+  var GeomToolbarModel = toolbar.Model.extend({
 
-        appendSelector: '#toolbar',
+    appendSelector: '#toolbar',
 
-        initialize: function(attributes) {
-            toolbar.Model.prototype.initialize.call(this, attributes);
-            commandStack.on('beforePop', this.beforePop, this);
-            VertexMV.eventProxy.on('committedCreate', this.committedCreate, this);
-            VertexMV.eventProxy.on('cancelledCreate', this.setToSelect, this);
-        },
+    initialize: function(attributes) {
+      toolbar.Model.prototype.initialize.call(this, attributes);
+      commandStack.on('beforePop', this.beforePop, this);
+      VertexMV.eventProxy.on('committedCreate', this.committedCreate, this);
+      VertexMV.eventProxy.on('cancelledCreate', this.setToSelect, this);
+    },
 
-        activate: function(item) {
-            if (item !== this.activeItem) {
-                var savedSelection = selection.getSelected();
-                this.deactivateActiveItem();
+    activate: function(item) {
+      if (item !== this.activeItem) {
+        var savedSelection = selection.getSelected();
+        this.deactivateActiveItem();
 
-                // Don't re-enter on cancel
-                VertexMV.eventProxy.off('cancelledCreate', this.setToSelect, this);
-                selection.deselectAll();
-                modelGraph.cancelIfEditing();
-                VertexMV.eventProxy.on('cancelledCreate', this.setToSelect, this);
+        // Don't re-enter on cancel
+        VertexMV.eventProxy.off('cancelledCreate', this.setToSelect, this);
+        selection.deselectAll();
+        modelGraph.cancelIfEditing();
+        VertexMV.eventProxy.on('cancelledCreate', this.setToSelect, this);
 
-                this.setActive(item, savedSelection);
-            }
-        },
+        this.setActive(item, savedSelection);
+      }
+    },
 
-        setActive: function(item, savedSelection) {
-            this.activeItem = item;
-            item.activate(savedSelection)
-        },
+    setActive: function(item, savedSelection) {
+      this.activeItem = item;
+      item.activate(savedSelection)
+    },
 
-        deactivateActiveItem: function() {
-            if (this.activeItem) {
-                this.activeItem.deactivate();
-            }
-        },
+    deactivateActiveItem: function() {
+      if (this.activeItem) {
+        this.activeItem.deactivate();
+      }
+    },
 
-        itemClicked: function(item) {
-            this.activate(item);
-        },
+    itemClicked: function(item) {
+      this.activate(item);
+    },
 
-        // Cancel any active tools when geometry is popped
-        beforePop: function() {
-            this.setToSelect();
-            selection.deselectAll();
-        },
+    // Cancel any active tools when geometry is popped
+    beforePop: function() {
+      this.setToSelect();
+      selection.deselectAll();
+    },
 
-        committedCreate: function(committedVertices) {
-            // The first vertex in the list is by convention the main vertex 
-            if (!committedVertices[0].implicit) {
-                for (var i = 0; i < this.items.length; ++i) {
-                    var item = this.items[i];
-                    if (item.createAnother && item.createAnother(committedVertices[0].type)) {
-                        this.setActive(item, []);
-                        return;
-                    }
-                }
-                this.setToSelect();
-            }
+    committedCreate: function(committedVertices) {
+      // The first vertex in the list is by convention the main vertex 
+      if (!committedVertices[0].implicit) {
+        for (var i = 0; i < this.items.length; ++i) {
+          var item = this.items[i];
+          if (item.createAnother && item.createAnother(committedVertices[0].type)) {
+            this.setActive(item, []);
+            return;
+          }
+        }
+        this.setToSelect();
+      }
 
-        },
+    },
 
-        setToSelect: function() {
-            this.activate(selectItemModel);
-        },
+    setToSelect: function() {
+      this.activate(selectItemModel);
+    },
 
-    });
+  });
 
-    var toolbarModel = new GeomToolbarModel({name: 'geometry'});
-    toolbarModel.addItem(selectItemModel);
-    toolbarModel.setToSelect();
-    return toolbarModel;
+  var toolbarModel = new GeomToolbarModel({name: 'geometry'});
+  toolbarModel.addItem(selectItemModel);
+  toolbarModel.setToSelect();
+  return toolbarModel;
 
 });
