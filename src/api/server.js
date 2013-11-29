8e6977dd7209f103c98351389143eebcbd0afc32
Charles de MAGNEVAL <magneval@gmail.com>
2013-11-29 10:29:23 +0100

    Update server.js
    remove useless comment

--- src/api/server.js
+++ src/api/server.js
@@ -2,8 +2,6 @@ var http = require('http');
 var app = require('./app');
 var nconf = require('nconf');
 
-
-//var port = nconf.get('port');
 var port = process.env.PORT || nconf.get('port');
 var server = http.createServer(app);
 server.listen(port, function() {
