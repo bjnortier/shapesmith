64c08e77d200ce5658f33e5d8326491eab0f1be2
Ben Nortier <bjnortier@gmail.com>
2014-05-22 16:45:33 +0000

    fix rqeuirejs config

--- Gruntfile.js
+++ Gruntfile.js
@@ -108,7 +108,7 @@ module.exports = function(grunt) {
               name: "worker",
             }
           ],
-          fileExclusionRegExp: /(^\.|^bin$|^artwork$|^.*.db$|^.db$|^test|^grunt.*$|^mocha$|^chai$|^webdriverjs$)/,
+          fileExclusionRegExp: /(^\.|^build.*$|^bin$|^artwork$|^.*.db$|^.db$|^test|^grunt.*$|^mocha$|^chai$|^webdriverjs$)/,
         }
       }
     },
